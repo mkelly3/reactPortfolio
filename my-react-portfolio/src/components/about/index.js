9bb13a0bf98737dbb035850e0d2ab24762400d21
Morgan Kelly <mkelly5380@gmail.com>
2022-12-02 10:30:33 -0600

    added about section in portfolio

--- my-react-portfolio/src/components/about/index.js
+++ my-react-portfolio/src/components/about/index.js
@@ -1,1 +1,33 @@
-cd
+import React from 'react';
+
+
+function About() {
+    return (
+        <div className ="about">
+            <div>
+            <h1 className='name'>Morgan Kelly</h1>
+            <p> Software Developer with a demonstrated history of working on production web and mobile applications. Deep
+                understanding and technical expertise in Computer Science fundamentals, Javascript, ReactJS, NodeJS, MongoDB, CSS,
+                HTML and other technologies.</p>
+            </div>
+
+            <div className='education'>
+            <h2 className='edu'>Education</h2>
+            <br />
+
+            <div className='grid-container-profile'>
+                <div className='grid-item'>
+                    <div className='title-uni'>University Of California, Berkeley</div>
+                        <div className='title-concentration'>Full Stack Web Development</div>
+            </div>
+            <div className="grid-item">
+                <div className='title-uni'>Purdue University</div>
+                    <div className='title-concentration'>Bachelor of Science: Biological Sciences</div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
