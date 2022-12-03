b72acce4b8ccc74739ecc4937b012a3fd77282cf
Morgan Kelly <mkelly5380@gmail.com>
2022-12-03 11:56:09 -0600

    added projects to portfolio.json

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-function Navigation(props) {   
-
+function Navbar() {
     return (
-    <div className=" row navigation" id="navigation">
-        <NavLink to="/about">ABOUT </NavLink>
-        <NavLink to="/portfolio">PORTFOLIO </NavLink>
-        <NavLink to="/contact">CONTACT </NavLink>
-        <NavLink to="/resume">RESUME</NavLink>
-    </div>
-  );
+        <div className='topnav'>
+            <div className='topnav-left'>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/about"
+                >
+                    ABOUT
+                </NavLink>
+            </div>
+            <div className='topnav-right'>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/portfolio"
+                >
+                    PORTFOLIO
+                </NavLink>
+              
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/contact"
+                >
+                    CONTACT
+                </NavLink>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/resume"
+                >
+                    RESUME
+                </NavLink>
+            </div>
+        </div>
+    );
 }
 
-export default Navigation;
+export default Navbar;
